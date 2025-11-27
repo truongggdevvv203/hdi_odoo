e58ec79de7766a2ef73732ce7f0d75572659a30f
truongggdevvv203 <Co.vt212482@sis.hust.edu.vn>
2025-11-27 03:10:29 +0000

    27.6.2

--- hdi_crm/models/crm_stage.py
+++ hdi_crm/models/crm_stage.py
@@ -86,6 +86,12 @@ class CrmTeam(models.Model):
         default=True,
     )
     
+    company_id = fields.Many2one(
+        'res.company',
+        string='Company',
+        default=lambda self: self.env.company,
+    )
+    
     member_ids = fields.Many2many(
         'res.users',
         string='Team Members',
