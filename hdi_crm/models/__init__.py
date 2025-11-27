5407ecb7b172f0620a7ff46a197d21d3a3d9de67
truong minh <mtruongg203@gmail.com>
2025-11-27 16:59:13 +0700

    2

--- hdi_crm/models/__init__.py
+++ hdi_crm/models/__init__.py
@@ -1,9 +1,9 @@
-from . import crm_lead
-from . import crm_opportunity
-from . import crm_customer
-from . import crm_activity
-from . import crm_stage
-from . import crm_tag
-from . import crm_partner_enhancement
-from . import crm_lead_enhancement
-from . import crm_opportunity_enhancement
+# from . import crm_lead
+# from . import crm_opportunity
+# from . import crm_customer
+# from . import crm_activity
+# from . import crm_stage
+# from . import crm_tag
+# from . import crm_partner_enhancement
+# from . import crm_lead_enhancement
+# from . import crm_opportunity_enhancement
