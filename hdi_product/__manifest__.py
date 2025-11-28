17cdeb102a985b1d451b5666bbcc4e52859b63c0
truongggdevvv203 <Co.vt212482@sis.hust.edu.vn>
2025-11-28 03:12:25 +0000

    Add HDI Product module with custom fields, views, and menu items

---
+++ hdi_product/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'HDI Product',
+    'version': '18.0.1.0.0',
+    'summary': 'HDI extensions for Odoo Product (inheritance only)',
+    'category': 'hdi',
+    'description': 'Inherit Odoo base product models to add HDI custom fields and views. Does NOT create new models with conflicting table names.',
+    'author': 'HDI Development Team',
+    'license': 'LGPL-3',
+    'depends': [
+        'product',
+        'uom',
+    ],
+    'data': [
+        'views/menu.xml',
+        'views/product_category_views.xml',
+        'views/product_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
