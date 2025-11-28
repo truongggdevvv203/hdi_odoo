f8ad2505b3cb86fd4214f18cba411139050eaa8f
truong minh <mtruongg203@gmail.com>
2025-11-28 10:22:04 +0700

    3

--- hdi_product/models/product_category.py
+++ hdi_product/models/product_category.py
@@ -1,7 +1,7 @@
 from odoo import models, fields
 
-
 class ProductCategory(models.Model):
     _inherit = 'product.category'
 
-  
+    hdi_description = fields.Text("Category Description")
+    hdi_image = fields.Binary("Image")
--- hdi_product/models/product_template.py
+++ hdi_product/models/product_template.py
@@ -1,6 +1,8 @@
 from odoo import models, fields
 
-
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
+    hdi_brand = fields.Char("Brand")
+    hdi_warranty_months = fields.Integer("Warranty (months)")
+    hdi_note = fields.Text("Additional Notes")
