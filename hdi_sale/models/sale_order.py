2c31b8107e12dbc49f95590f4a0c09402c35ab21
truong minh <mtruongg203@gmail.com>
2025-12-02 15:07:57 +0700

    Update sale_order.py

--- hdi_sale/models/sale_order.py
+++ hdi_sale/models/sale_order.py
@@ -98,6 +98,7 @@ class SaleOrder(models.Model):
         """Compute base shipping cost from the selected main shipping service"""
         for order in self:
             order.base_shipping_cost = int(order.shipping_service_id.base_price) if order.shipping_service_id else 0
+
     @api.depends('base_shipping_cost', 'cod_amount', 'receiver_pay_fee', 'additional_cost')
     def _compute_total_shipping_fee(self):
         """Calculate total fee = base shipping + additional services + COD fee"""
