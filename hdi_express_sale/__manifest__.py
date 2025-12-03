0e2735028c1d9067b3df7553986989f76cc7fc67
truong minh <mtruongg203@gmail.com>
2025-12-03 16:30:06 +0700

    312 v5

--- hdi_express_sale/__manifest__.py
+++ hdi_express_sale/__manifest__.py
@@ -12,7 +12,7 @@
         'views/express_shipping_service_views.xml',
         'views/express_shipping_order_views.xml',
         'views/express_order_search_views.xml',
-        'views/express_debt_reconciliation_views.xml',
+        # 'views/express_debt_reconciliation_views.xml',
         'views/menu.xml',
     ],
     'assets': {
--- hdi_express_sale/models/__init__.py
+++ hdi_express_sale/models/__init__.py
@@ -3,4 +3,4 @@ from . import express_shipping_order
 from . import express_dashboard
 from . import express_res_users
 from . import express_order_search
-from . import express_debt_reconciliation
+# from . import express_debt_reconciliation
