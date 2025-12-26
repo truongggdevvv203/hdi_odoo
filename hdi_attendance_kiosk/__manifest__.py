e8f42952db7f0d9a529c4e74d615d5e7c005b27f
truongggdevvv203 <Co.vt212482@sis.hust.edu.vn>
2025-12-26 07:02:14 +0000

    feat: Thêm module Kiosk chấm công với giao diện và API tương tác

---
+++ hdi_attendance_kiosk/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'HDI Attendance Kiosk',
+    'version': '1.0.0',
+    'category': 'Human Resources',
+    'description': 'Attendance check-in/check-out kiosk interface',
+    'author': 'HDI',
+    'license': 'AGPL-3',
+    'depends': ['hr_attendance', 'web'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/attendance_kiosk_views.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'hdi_attendance_kiosk/static/src/js/attendance_kiosk.js',
+            'hdi_attendance_kiosk/static/src/css/attendance_kiosk.css',
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+}
---
+++ hdi_attendance_kiosk/static/src/js/attendance_kiosk.js
@@ -0,0 +1,2 @@
+// Attendance Kiosk JavaScript
+// Phục vụ chức năng chấm công tương tác
